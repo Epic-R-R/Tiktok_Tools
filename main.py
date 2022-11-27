287d174eaf750fb5a671c5e2149ad107ef170abc
Epic-R-R <epicrr001@gmail.com>
2022-11-27 14:37:32 +0300

    Add some if statement chech requests and username found

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+
+
 
 def get_content(url: str, payload: dict) -> bytes:
     response = requests.post(url=url, data=payload)
     if response.status_code == 200:
         return response.content
     else:
-        return 404
+        return 0
 
 def download_img(username: str, content: bytes) -> None:
     soup = BeautifulSoup(content, 'html.parser')
     img = soup.findAll('img')[-1]['src']
+    if os.path.basename(img) == "share_img.png":
+        username = f"{username}_not_found"
     response = requests.get(img, stream=True)
     with open(f"{username}.jpg", 'wb') as fr:
         for chunk in response.iter_content(chunk_size=128):
@@ -20,4 +25,7 @@ if __name__ == "__main__":
     url = "https://www.howtotechies.com/pinterest-video-downloader"
     username = input("Enter username: ")
     payload = {"video-url": username}
-    download_img(username=username, content=get_content(url=url, payload=payload))
+    if get_content(url=url, payload=payload) == 0:
+        print("Bad requests, Try again later.")
+    else:
+        download_img(username=username, content=get_content(url=url, payload=payload))
